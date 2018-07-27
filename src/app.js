806b8070260b832fa2c7b36e728131e3f330c361
kelvinwang <kv.wang87@gmail.com>
2018-07-27 18:26:13 +0800

    make store

--- src/app.js
+++ src/app.js
@@ -11,24 +11,27 @@ import Topics from './pages/Topics';
 
 
 const store = new configurestore();
-const App = () => {
-    return (
-        <div>
-            当前用户：<span>fx</span>
-            <Router>
-                <div>
-                    <ul>
-                        <li> <Link to="/">Home1</Link> </li>
-                        <li> <Link to="/about">About</Link> </li>
-                        <li> <Link to="/topics">Topics</Link> </li>
-                    </ul>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/about" component={About} />
-                    <Route path="/topics" component={Topics} />
-                </div>
-            </Router>
-        </div>
-    );
+
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                当前用户：<span>fx</span>
+                <Router>
+                    <div>
+                        <ul>
+                            <li> <Link to="/">Home1</Link> </li>
+                            <li> <Link to="/about">About</Link> </li>
+                            <li> <Link to="/topics">Topics</Link> </li>
+                        </ul>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/about" component={About} />
+                        <Route path="/topics" component={Topics} />
+                    </div>
+                </Router>
+            </div>
+        );
+    }
 };
 
 console.log(store);
