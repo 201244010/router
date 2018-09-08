44103e41bb7782b9f907cca24c36cb0d8a51245c
189 <>
2018-09-08 15:01:41 +0800

    settings

--- src/components/PrimaryHeader/index.jsx
+++ src/components/PrimaryHeader/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
 import './header.scss';
+import classnames from 'classnames';
 import SubLayout from "../SubLayout";
 import Icon from '~/components/Icon';
 
 export default class Header extends React.Component {
 	constructor(props) {
-		super(props);
+        super(props);
 	}
 
 	render() {
+        const logined = this.props.logined;
 		return (
 			<div className="header">
 				<SubLayout>
@@ -20,16 +22,20 @@ export default class Header extends React.Component {
                             </div>
 							<span className="ui-ib slogan">SUNMI W1</span>
 						</li>
-						<li className="menu ui-hidden">
-							<a href="javascript:;" className="ui-ib now">网络状态</a>
-							<a href="javascript:;" className="ui-ib">基础设置</a>
-							<a href="javascript:;" className="ui-ib">高级设置</a>
-						</li>
-						<li className="sidebar ui-hidden">
-							<a href="javascript:;" className="ui-ib">下载手机版</a>
-							<span className="ui-ib">|</span>
-							<a href="javascript:;" className="ui-ib">退出管理</a>
-						</li>
+						{
+                            logined ? [
+                                <li key="1" className="menu">
+                                    <a href="javascript:;" className="ui-ib now">网络状态</a>
+                                    <a href="javascript:;" className="ui-ib">基础设置</a>
+                                    <a href="javascript:;" className="ui-ib">高级设置</a>
+                                </li>,
+                                <li key="2" className="sidebar">
+                                    <a href="javascript:;" className="ui-ib">下载手机版</a>
+                                    <span className="ui-ib">|</span>
+                                    <a href="javascript:;" className="ui-ib">退出管理</a>
+                                </li>
+                            ] : ''
+                        }
 					</ul>
 				</SubLayout>
 			</div>
---
+++ src/pages/Settings/index.jsx
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import SubLayout from '~/components/SubLayout';
+
+
+
+export default class Setting extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <SubLayout className="settings">
+                <div>基础设置</div>
+            </SubLayout>
+        )
+    }
+};
+
+
+
+
+
+
+
