4a25b482b596ed57986729c57dd19a0847f9c779
liguanghua <liguanghua@sunmi.com>
2018-09-26 16:29:53 +0800

    modify home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,10 @@
 
 import React from 'react';
-import classnames from 'classnames';
-import { Button, Table, Divider, Popconfirm, Modal } from 'antd';
+import { Button, Table, Divider, Popover, Modal } from 'antd';
 import SubLayout from '~/components/SubLayout';
 import PrimaryFooter from '~/components/PrimaryFooter';
 import CustomIcon from '~/components/Icon';
+import ClientList from './ClientList';
 
 import './home.scss'
 
@@ -16,183 +16,215 @@ export default class Home extends React.PureComponent {
         normalNum:21,
         sunmiClients:[{
             name:'Bla Bla Bla Bla Bla Bla Bla Bla',
+            ip:'192.168.100.1',
             mac:'00:11:22:33:44:00',
             icon:'computer',
             type:'sunmi'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip:'192.168.100.1',
             mac:'00:11:22:33:44:01',
             icon: 'bootdevice',
             type: 'sunmi'
         },
         {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:02',
             icon: 'bootdevice',
             type: 'sunmi'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:03',
             icon: 'pad',
             type: 'sunmi'
         }],
         normalClients: [{
             name:'Bla Bla Bla Bla Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac:'00:11:22:33:44:00',
             icon:'computer',
-            type:'sunmi'
+            type:'normal'
         }, {
             name: 'Bla Bla Bla Bla',
+                ip: '192.168.100.1',
             mac:'00:11:22:33:44:01',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'normal'
         },
         {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:02',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'normal'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:03',
             icon: 'pad',
-            type: 'sunmi'
+            type: 'normal'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:04',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'normal'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:05',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'normal'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:06',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'normal'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:07',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'normal'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:08',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'normal'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:09',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'normal'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:0A',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'normal'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:0B',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'normal'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:0C',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'normal'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:0D',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'normal'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:0E',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'normal'
         }],
         priorityClients: [{
             name:'Bla Bla Bla Bla Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac:'00:11:22:33:44:00',
             icon:'computer',
-            type:'sunmi'
+            type:'priority'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip:'192.168.100.1',
             mac:'00:11:22:33:44:01',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'priority'
         },
         {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:02',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'priority'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:03',
             icon: 'pad',
-            type: 'sunmi'
+            type: 'priority'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:04',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'priority'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:05',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'priority'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:06',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'priority'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:07',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'priority'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:08',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'priority'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:09',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'priority'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:0A',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'priority'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:0B',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'priority'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:0C',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'priority'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:0D',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'priority'
         }, {
             name: 'Bla Bla Bla Bla',
+            ip: '192.168.100.1',
             mac: '00:11:22:33:44:0E',
             icon: 'bootdevice',
-            type: 'sunmi'
+            type: 'priority'
         }]
     }
-
-
 
     render(){
         const { sunmiClients, normalClients, priorityClients}  = this.state;
@@ -201,18 +233,46 @@ export default class Home extends React.PureComponent {
             <div>
                 <SubLayout>
                     <ul className='func-list'>
+                        <li className='func-item internet'>
+                            <img className='router-bg' src={require('~/assets/images/router-bg.png')} />
+                            <img className='router' src={require('~/assets/images/router.svg')} />
+                            <div className='content'>
+                                <h4><span>网络状态</span><span className='state'>正常</span></h4>
+                                <div className='down'>
+                                    <label className='speed'>6.25</label>
+                                    <div className='tip'>
+                                        <span>上行带宽</span><CustomIcon type='kbyte' size={12} color='#3D76F6' />
+                                        <div className='unit'>Mbps</div>
+                                    </div>
+                                </div>
+                                <div className='up'>
+                                    <label className='speed'>6.25</label>
+                                    <div className='tip'>
+                                        <span>下行带宽</span><CustomIcon type='downloadtraffic' size={12} color='#87D068' />
+                                        <div className='unit'>Mbps</div>
+                                    </div>
+                                </div>
+                                <Button className='test-speed'>自动测速</Button>
+                            </div>
+                        </li>
                         <li className='func-item'></li>
-                        <li className='func-item'></li>
-                        <li className='func-item'></li>
+                        <li className='func-item search'>
+                            <img className='radar' src={require('~/assets/images/radar.png')} />
+                            <div className='content'>
+                                <h3>搜寻商米设备</h3>
+                                <p>一键添加附近的<br />商米设备专属网络业务不掉线</p>
+                                <Button className='search'>搜寻设备</Button>
+                            </div>
+                        </li>
                     </ul>
                     <p className='online-clinet'>在线设备（<span>{total}</span>）</p>
                     <div className='online-list'>
                         <div className='left-list'>
-                            <ClientList className='sunmi-list' data={sunmiClients} />
-                            <ClientList className='normal-list' data={normalClients} />
+                            <ClientList type='sunmi' data={sunmiClients} placement='top' />
+                            <ClientList type='normal' data={normalClients} placement='top' />
                         </div>
                         <div className='priority-list'>
-                            <ClientList className='priority-list' data={priorityClients} />
+                            <ClientList type='priority' data={priorityClients} placement='top' />
                         </div>
                     </div>
                 </SubLayout>
@@ -221,41 +281,3 @@ export default class Home extends React.PureComponent {
         );
     }
 };
-
-const ClientList  = (props) => {
-    const clients = props.data;
-    const total = clients.length;
-    let maxConf = {
-        'sunmi-list':6,
-        'normal-list':12,
-        'priority-list':12
-    };
-    let deviceTypeMap = {
-        'sunmi-list': '商米设备',
-        'normal-list': '普通设备',
-        'priority-list': '优先设备'
-    };
-    const deviceType = deviceTypeMap[props.className];
-    const max = parseInt(maxConf[props.className], 10);
-    const current = (total < max) ? total : max;
-
-    const listItems = clients.map((client, index) => {
-        if (index < max){
-            return (
-                <li key={client.mac} className='client-item'>
-                    <div className='icon'><CustomIcon type={client.icon} size={24} /></div>
-                    <div className='dot'></div><p title={client.name}>{client.name}</p>
-                </li>
-            );
-        }
-    });
-
-    return (
-        <div className={classnames(['list-content', props.className])}>
-            <div className='list-header'>
-                <Divider type="vertical" className='divider' /><span>{deviceType}</span><span className='statistics'>（{current}/{total}）</span><Button className='more'>查看全部</Button>
-            </div>
-            <ul>{listItems}</ul>
-    </div>);
-}
-
