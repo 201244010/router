6a9b4d3db935cc041448b83e4f0c9aeca9a193d8
liguanghua <liguanghua@sunmi.com>
2018-10-12 16:42:50 +0800

    add DoS page

--- src/pages/Advance/Systemsetup/index.jsx
+++ src/pages/Advance/Systemsetup/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Tabs} from 'antd';
+import Dosd from './Dosd';
 import ChangePassword from './ChangePassword';
 import SysUpgrade from './SysUpgrade';
 import Backup from './Backup';
@@ -14,7 +15,8 @@ const TabPane = Tabs.TabPane;
 export default class Systemsetup extends React.PureComponent {
     render(){
         return (
-            <Tabs className="weixin-tabs" defaultActiveKey="ChangePassword">
+            <Tabs>
+                <TabPane tab="攻击防护" key="Dosd"><Dosd /></TabPane>
                 <TabPane tab="修改管理员密码" key="ChangePassword"><ChangePassword /></TabPane>
                 <TabPane tab="系统升级" key="SysUpgrade"><SysUpgrade /></TabPane>
                 <TabPane tab="备份与恢复" key="Backup"><Backup /></TabPane>
