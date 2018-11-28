1a4f79a56f5c0a3c0ffa8e34f3309c830fe42d13
liguanghua <liguanghua@sunmi.com>
2018-11-28 18:19:10 +0800

    [Home]add table sorter & filter

---
+++ src/assets/common/utils.js
@@ -0,0 +1,55 @@
+
+let formatTime = (total) => {
+    let seconds = parseInt(total, 10);
+    let day = parseInt(seconds / 86400);
+    let hour = parseInt((seconds % 86400) / 3600);
+    let minute = parseInt((seconds % 3600) / 60);
+    let second = parseInt(seconds % 60);
+
+    let timeStr = "";
+    if (day > 0) {
+        timeStr += day + "天";
+    }
+
+    if (hour > 0) {
+        timeStr += hour + "时";
+    }
+
+    if (minute > 0) {
+        timeStr += minute + "分";
+    }
+
+    if (second >= 0) {
+        timeStr += second + "秒";
+    }
+
+    return timeStr;
+}
+
+// 格式化网络速率，最多保留4位数字+单位
+let formatSpeed = (speed) => {
+    let kSpeed = 1024;
+    let mSpeed = kSpeed * 1024;
+    let gSpeed = mSpeed * 1024;
+    // 'xx.xx'
+
+    speed = parseInt(speed, 10);
+    if (speed >= gSpeed) {
+        let val = speed / gSpeed;
+        speed = (val).toFixed(val > 99 ? 0 : 2) + "GB/s";
+    }
+    else if (speed >= mSpeed) {
+        let val = speed / mSpeed;
+        speed = (val).toFixed(val > 99 ? 0 : 2) + "MB/s";
+    }
+    else if (speed >= kSpeed) {
+        speed = (speed / kSpeed).toFixed(0) + "KB/s";
+    }
+    else {
+        speed = speed.toFixed(0) + "B/s";
+    }
+
+    return speed + '';
+}
+
+export { formatTime, formatSpeed };
