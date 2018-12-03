1ea2f2397a0880844673ac5548ebbf0eee4aaf29
liujun <3038855308@qq.com>
2018-12-03 14:17:32 +0800

    高级设置-网速智能配置 样式修改

--- src/pages/Advance/Bandwidth/index.jsx
+++ src/pages/Advance/Bandwidth/index.jsx
@@ -387,14 +387,24 @@ export default class Bandwidth extends React.PureComponent {
                         <PanelHeader title="总带宽" checkable={false} onChange={(value)=>this.onChange('channelType',value)}/>
                     </section>
                     <section className="band-value">
-                        <div className="band-size">{upband}
-                            <span className="band-unit">{unit}</span>
-                            <span className="band-bottom">上行带宽<span className="icon-band"><CustomIcon size={12} color="#3D76F6" type="kbyte"/></span></span>
+                        <div className="band-size">
+                            <div className="band-left">{upband}</div>
+                            <div className="band-right">
+                                <span className="band-unit">{unit}</span>
+                                <span className="band-unit">上行带宽
+                                    <span style={{marginLeft: 8}}><CustomIcon style={{paddingBottom:3}} size={12} color="#3D76F6" type="kbyte"/></span>
+                                </span>
+                            </div>
                         </div>
                         <div className="band-line"></div> 
-                        <div className="band-size">{downband}
-                            <span className="band-unit">{unit}</span>
-                            <span className="band-bottom">下行带宽<span className="icon-band"><CustomIcon size={12} color="#87D068" type="downloadtraffic"/></span></span>
+                        <div className="band-size">
+                            <div className="band-left">{downband}</div>
+                            <div className="band-right">
+                                <span className="band-unit">{unit}</span>
+                                <span className="band-unit">下行带宽
+                                    <span style={{marginLeft: 8}}><CustomIcon style={{paddingBottom:3}} size={12} color="#87D068" type="downloadtraffic"/></span>
+                                </span>
+                            </div>
                         </div>
                     </section>
                     <section style={{margin:"16px 20px 32px 0"}}>
