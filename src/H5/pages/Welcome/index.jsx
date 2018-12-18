d234158dc620e58f23962aab7cb3962dbaa043a6
liguanghua <liguanghua@sunmi.com>
2018-12-18 15:27:55 +0800

    [welcome]更新用户协议和隐私政策

--- src/H5/pages/Welcome/index.jsx
+++ src/H5/pages/Welcome/index.jsx
@@ -5,6 +5,9 @@ import Link from 'h5/components/Link';
 import Checkbox from 'h5/components/Checkbox';
 import Modal from 'h5/components/Modal';
 
+import Agreement from './Agreement';
+import Privacy from './Privacy';
+
 import { UA, PAGE_STYLE_KEY, PAGE_STYLE_WEB } from '~/utils';
 
 import './welcome.scss';
@@ -81,24 +84,14 @@ export default class Home extends React.Component {
                     visible={visible}
                     footer={<div onClick={this.iknow}>我知道了</div>}
                 >
-                    <div className='policy-content'>
-                        欢迎您使用“商米”软件及相关服务！
-为了更好地为您提供服务，请您仔细阅读本《用户服务协议及隐私保护政策》（以下简称“本协议”）。在您开始使用“商米”软件及相关服务之前，请您务必认真阅读并充分理解本协议，特别是涉及免除或者限制责任的条款、权利许可和信息使用的条款、同意开通和使用特殊单项服务的条款、法律适用和争议解决条款等。其中，免除或者限制责任条款等重要内容将以加粗形式提示您注意，您应重点阅读。如您未满18周岁，请您在法定监护人陪同下仔细阅读并充分理解本协议，并征得法定监护人的同意后下载本软件。
-除非您完全接受本协议的全部内容，否则您无权下载、安装、注册、登录、使用（以下统称“使用”）“商米”软件，或者通过任何方式使用“商米”服务，或者获得“商米”软件提供的任何服务 （本协议中统称“使用”）。若您使用“商米”软件及相关服务
-则视为您已充分理解本协议并承诺作为本协议的一方当事人接受协议的约束。
-                    </div>
+                    <Agreement />
                 </Modal>
                 <Modal
                     title='隐私政策'
                     visible={policy}
                     footer={<div onClick={this.iknow}>我知道了</div>}
                 >
-                    <div className='policy-content'>
-                        欢迎您使用“商米”软件及相关服务！
-为了更好地为您提供服务，请您仔细阅读本《用户服务协议及隐私保护政策》（以下简称“本协议”）。在您开始使用“商米”软件及相关服务之前，请您务必认真阅读并充分理解本协议，特别是涉及免除或者限制责任的条款、权利许可和信息使用的条款、同意开通和使用特殊单项服务的条款、法律适用和争议解决条款等。其中，免除或者限制责任条款等重要内容将以加粗形式提示您注意，您应重点阅读。如您未满18周岁，请您在法定监护人陪同下仔细阅读并充分理解本协议，并征得法定监护人的同意后下载本软件。
-除非您完全接受本协议的全部内容，否则您无权下载、安装、注册、登录、使用（以下统称“使用”）“商米”软件，或者通过任何方式使用“商米”服务，或者获得“商米”软件提供的任何服务 （本协议中统称“使用”）。若您使用“商米”软件及相关服务
-则视为您已充分理解本协议并承诺作为本协议的一方当事人接受协议的约束。
-                    </div>
+                    <Privacy />
                 </Modal>
                 <div className='copyright'>© 2018 上海商米科技有限公司</div>
                 <Icon type='earth' size={'10.6667rem'} style={{
