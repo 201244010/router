4bcf3435a28d3529f9f3028e1ddd59146d743aac
liujun <3038855308@qq.com>
2018-12-26 15:00:46 +0800

    WeChatPreview组件修改

--- src/pages/Advance/Auth/WeChatAuth.jsx
+++ src/pages/Advance/Auth/WeChatAuth.jsx
@@ -462,49 +462,13 @@ export default class WeChatAuth extends React.Component{
                             <section>
                                 <WeChatPreview
                                     style={{marginTop:20}}
-                                    bg_img={bg_img}
-                                    logo_img={logo_img}
+                                    BgImage={bg_img}
+                                    logoImage={logo_img}
                                     logo={logo}
                                     welcome={welcome}
-                                    loginHint={loginHint}
+                                    buttonInfo={loginHint}
                                     statement={statement}
                                 />
-                                {/* <div style={{
-                                    display:'block',
-                                    width:325,
-                                    height:488,
-                                    borderRadius:8,
-                                    marginTop:25,
-                                    padding:'73px 0 0 0',
-                                    color:'#FFFFFF',   
-                                    backgroundImage: 'linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)),'+`url(${bg_img})`,
-                                    backgroundRepeat:'no-repeat',
-                                    backgroundSize: 'cover',
-                                    backgroundPosition:'center',
-                                    }}>
-                                    
-                                    <div style={{paddingLeft:20,height:383}}>
-                                        <div style={{
-                                            width:52,
-                                            height:52,
-                                            border:'2px solid #FFFFFF',
-                                            borderRadius:26,
-                                            backgroundImage: `url(${logo_img})`,
-                                            backgroundRepeat:'no-repeat',
-                                            backgroundSize: '100% 100%', 
-                                            }}></div>
-                                        <div style={{minHeight:25,marginTop:17,fontSize:18}}>{logo}</div>
-                                        <div style={{minHeight:33,marginTop:18,fontSize:24}}>{welcome}</div>
-                                        <div style={{marginTop:18}}>
-                                            <Button type="primary" style={{width:286,height:40,fontSize:14,borderRadius:8}}>{loginHint}</Button>
-                                        </div>
-                                        <div>
-                                            <Checkbox checked={true} style={{fontSize:10,color:'#FFFFFF'}}><span style={{opacity: 0.8}}>我已阅读并同意《上网协议》</span></Checkbox>
-                                        </div>
-                                    </div>
-                                    <div style={{postion:'relative',textAlign:'center',color:'#ADB1B9',opacity: 0.8,fontSize:12}}>©{statement}</div>
-                                    
-                                </div> */}
                             </section>                  
                         </section>
                         <PanelHeader
--- src/pages/Advance/Auth/WeChatPreview/index.jsx
+++ src/pages/Advance/Auth/WeChatPreview/index.jsx
@@ -9,27 +9,27 @@ export default class WeChatPreview extends React.Component {
     }
 
     render() {
-        const { style, bg_img, logo_img, logo, welcome, loginHint, statement } = this.props;
+        const { style, BgImage, logoImage, logo, welcome, buttonInfo, statement } = this.props;
 
         return (
             <div className='preview-outline' style={style}>
                 <label style={{margin: 0}}>预览效果</label>
                 <div
                     className='preview-content'
-                    style={{backgroundImage: 'linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)),'+`url(${bg_img})`}}
+                    style={{backgroundImage: 'linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)),'+`url(${BgImage})`}}
                 > 
-                    <div className='preview-content-top'>
-                        <div className='preview-content-logo_img' style={{backgroundImage: `url(${logo_img})`}}></div>
-                        <div className='preview-content-logo'>{logo}</div>
-                        <div className='preview-content-welcome'>{welcome}</div>
-                        <div className='preview-content-loginHint'>
-                            <Button type="primary" className='preview-content-loginHint-button'>{loginHint}</Button>
+                    <div className='body'>
+                        <div className='logo-img' style={{backgroundImage: `url(${logoImage})`}}></div>
+                        <div className='logo'>{logo}</div>
+                        <div className='welcome'>{welcome}</div>
+                        <div className='button-outline'>
+                            <Button type="primary" className='button'>{buttonInfo}</Button>
                         </div>
                         <div>
-                            <Checkbox className='preview-content-checkbox' checked={true} ><span >我已阅读并同意《上网协议》</span></Checkbox>
+                            <Checkbox className='checkbox' checked={true} ><span >我已阅读并同意《上网协议》</span></Checkbox>
                         </div>
                     </div>
-                    <div className='preview-content-statement'>©{statement}</div>    
+                    <div className='footer'>©{statement}</div>    
                 </div>
             </div>
         );
