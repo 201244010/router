e2e54b8aa4abc60233902943295aaaf4af18e303
liujun <3038855308@qq.com>
2019-01-17 17:24:32 +0800

    根据产品定义，从网络获取切换会获取本机时间时，本机时间的显示为从无到有

--- src/pages/Advance/Systemsetup/TimeZone.jsx
+++ src/pages/Advance/Systemsetup/TimeZone.jsx
@@ -38,6 +38,10 @@ export default class TimeZone extends React.Component {
                     hostTime: getNowTimeStr(),
                 });
             }, 1000);
+        } else {
+            this.setState({
+                hostTime: '',
+            });
         }
     };
 
