67afa71af44f4c55076c7957f555a390ccfe3563
liujun <3038855308@qq.com>
2019-01-18 17:45:23 +0800

    Advance/index.js 中文转英文 完成

--- src/pages/Advance/index.jsx
+++ src/pages/Advance/index.jsx
@@ -11,6 +11,9 @@ import Blacklist from './Blacklist';
 import Bootdevice from './Bootdevice';
 import Bandwidth from './Bandwidth';
 import Systemsetup from './Systemsetup';
+import intl from '~/i18n/intl';
+
+const MODULE = 'advance';
 
 export default class Advance extends React.PureComponent {
     constructor(props){
@@ -25,31 +28,31 @@ export default class Advance extends React.PureComponent {
                     <NavLink to={match.path + "/bandwidth"} activeClassName="active">
                         <div className="nav-item">
                             <CustomIcon type="networkspeeddistribution" size={28}/>
-                            <span>网速智能分配</span>
+                            <span>{intl.get(MODULE, 0)}</span>
                         </div>
                     </NavLink>
                     <NavLink to={match.path + "/wechat"} activeClassName="active">
                         <div className="nav-item">
                             <CustomIcon type="auth" size={28} />
-                            <span>微信连Wi-Fi</span>
+                            <span>{intl.get(MODULE, 1)}</span>
                         </div>
                     </NavLink>
                     <NavLink to={match.path + "/whitelist"} activeClassName="active">
                         <div className="nav-item">
                             <CustomIcon type="bootdevice" size={28}/>
-                            <span>优先设备</span>
+                            <span>{intl.get(MODULE, 2)}</span>
                         </div>
                     </NavLink>
                     <NavLink to={match.path + "/blacklist"} activeClassName="active">
                         <div className="nav-item">
                             <CustomIcon type="blacklis" size={28}/>
-                            <span>防蹭网</span>
+                            <span>{intl.get(MODULE, 3)}</span>
                         </div>
                     </NavLink>
                     <NavLink to={match.path + "/systemsetup"} activeClassName="active">
                         <div className="nav-item">
                             <CustomIcon type="systemsetup" size={28}/>
-                            <span>系统设置</span>
+                            <span>{intl.get(MODULE, 4)}</span>
                         </div>
                     </NavLink>
                 </nav>
