4c083a027fa087c5af73db7b6565f36b3cb5403c
liujun <3038855308@qq.com>
2019-01-19 12:36:13 +0800

    Reboot.jsx 中文转英文 完成

--- src/pages/Advance/Systemsetup/Reboot.jsx
+++ src/pages/Advance/Systemsetup/Reboot.jsx
@@ -3,6 +3,9 @@ import { Button, Icon, Modal, message } from 'antd';
 import CustomIcon from '~/components/Icon';
 import Progress from '~/components/Progress';
 import { clear } from '~/assets/common/auth';
+import intl from '~/i18n/intl';
+
+const MODULE = 'reboot';
 
 export default class Reboot extends React.Component{
     constructor(props){
@@ -33,17 +36,22 @@ export default class Reboot extends React.Component{
                 });
             }, duration * 1000);
         } else {
-            message.error(`路由器重启失败[${errcode}]`);
+            // message.error(`路由器重启失败[${errcode}]`);
+            message.error(intl.get(MODULE, 0, {errcode}));    
         }
     }
 
     showModal = () => {
         Modal.confirm({
             centered: true,
-            title: '提示',
-            content: '确定要立即重启路由器？',
-            okText: '立即重启',
-            cancelText: '取消',
+            // title: '提示',
+            // content: '确定要立即重启路由器？',
+            // okText: '立即重启',
+            // cancelText: '取消',
+            title: intl.get(MODULE, 1),
+            content: intl.get(MODULE, 2),
+            okText: intl.get(MODULE, 3),
+            cancelText: intl.get(MODULE, 4),
             onOk: this.reboot,
         });
     }
@@ -57,15 +65,15 @@ export default class Reboot extends React.Component{
         return (
             <div>
                 <div style={{marginTop : 8}}>
-                    <span style={{marginLeft:60}}>说明：路由器系统将立即重新启动</span>
+                    <span style={{marginLeft:60}}>{intl.get(MODULE, 5)}</span>
                     <section style={{borderTop:0,marginTop:8}} className="system-save">
-                        <Button style={{width:116}} className="system-button" type="primary" onClick={this.showModal}>立即重启</Button>
+                        <Button style={{width:116}} className="system-button" type="primary" onClick={this.showModal}>{intl.get(MODULE, 6)}</Button>
                     </section>
                 </div>
                 {loadingActive &&
                     <Progress
                         duration={duration}
-                        title='正在重启路由器，请耐心等待...'
+                        title={intl.get(MODULE, 7)}
                     />
                 }
                 <Modal
@@ -73,10 +81,10 @@ export default class Reboot extends React.Component{
                     className='modal-center'
                     closable={false}
                     centered={true}
-                    footer={[<Button type="primary" onClick={this.login}>确定</Button>]}
+                    footer={[<Button type="primary" onClick={this.login}>{intl.get(MODULE, 8)}</Button>]}
                 >
                     <CustomIcon type="succeed" size={64} color='#87D068' style={{ marginTop: 20 }} />
-                    <h3 style={{ marginTop: 15 }}>重启成功，请重新连接无线网络</h3>
+                    <h3 style={{ marginTop: 15 }}>{intl.get(MODULE, 9)}</h3>
                 </Modal>
         </div>
         );
