67ddc8ed4ceebf5ef3d8159f2225d7e8eb4bcfc0
liujun <3038855308@qq.com>
2019-01-19 13:25:58 +0800

    Diagnose/index.jsx 中文转英文 完成

--- src/pages/Diagnose/index.jsx
+++ src/pages/Diagnose/index.jsx
@@ -2,8 +2,11 @@
 import React from 'react';
 import { Button, Timeline, Icon  } from 'antd';
 import CustomIcon from '~/components/Icon';
+import intl from '~/i18n/intl';
 
 import './diagnose.scss';
+
+const MODULE = 'diagnose';
 
 export default class Diagnose extends React.Component {
     constructor(props) {
@@ -18,7 +21,8 @@ export default class Diagnose extends React.Component {
         problem: '',
         reason: '',
         solution: '',
-        btnStr: '重新检测',
+        // btnStr: '重新检测',
+        btnStr: intl.get(MODULE, 0),
         solutionFunc: null,
         bgIcon: 'detection',
     }
@@ -36,10 +40,14 @@ export default class Diagnose extends React.Component {
                 this.setState({
                     status: 'warning',
                     bgIcon: 'defeated',
-                    problem: '网线异常',
-                    reason: 'WAN口未插入网线',
-                    solution: '请检查您的网线是否插好，或重启您的宽带猫，等待约2分钟，待宽带猫正常工作后，再重新检测',
-                    btnStr: '重新检测',
+                    // problem: '网线异常',
+                    // reason: 'WAN口未插入网线',
+                    // solution: '请检查您的网线是否插好，或重启您的宽带猫，等待约2分钟，待宽带猫正常工作后，再重新检测',
+                    // btnStr: '重新检测',
+                    problem: intl.get(MODULE, 1),
+                    reason: intl.get(MODULE, 2),
+                    solution: intl.get(MODULE, 3),
+                    btnStr: intl.get(MODULE, 4),
                     solutionFunc: this.start,
                 });
                 return;
@@ -54,7 +62,8 @@ export default class Diagnose extends React.Component {
                 }, 2000);
             });
         } else {
-            Modal.error({ title: '网络诊断失败', content: message, centered: true });
+            // Modal.error({ title: '网络诊断失败', content: message, centered: true });
+            Modal.error({ title: intl.get(MODULE, 5), content: message, centered: true });
         }
     }
 
@@ -71,10 +80,14 @@ export default class Diagnose extends React.Component {
                 this.setState({
                     status: 'warning',
                     bgIcon: 'defeated',
-                    problem: 'WAN口拨号异常',
-                    reason: '上网方式设置错误，或宽带账号密码错误',
-                    solution: '请重新设置上网方式，或联系您的宽带运营商处理',
-                    btnStr: '设置上网方式',
+                    // problem: 'WAN口拨号异常',
+                    // reason: '上网方式设置错误，或宽带账号密码错误',
+                    // solution: '请重新设置上网方式，或联系您的宽带运营商处理',
+                    // btnStr: '设置上网方式',
+                    problem: intl.get(MODULE, 6),
+                    reason: intl.get(MODULE, 7),
+                    solution: intl.get(MODULE, 8),
+                    btnStr: intl.get(MODULE, 9),
                     solutionFunc: this.setWan,
                 });
                 return;
@@ -89,7 +102,8 @@ export default class Diagnose extends React.Component {
                 }, 2000);
             });
         } else {
-            Modal.error({ title: '网络诊断失败', content: message, centered: true });
+            //Modal.error({ title: '网络诊断失败', content: message, centered: true });
+            Modal.error({ title: intl.get(MODULE, 10), content: message, centered: true });
         }
     }
 
@@ -106,10 +120,14 @@ export default class Diagnose extends React.Component {
                 this.setState({
                     status: 'warning',
                     bgIcon: 'defeated',
-                    problem: '无Internet服务',
-                    reason: '上网方式设置错误，或运营商服务异常',
-                    solution: '请重新设置上网方式，或联系您的宽带运营商处理',
-                    btnStr: '设置上网方式',
+                    // problem: '无Internet服务',
+                    // reason: '上网方式设置错误，或运营商服务异常',
+                    // solution: '请重新设置上网方式，或联系您的宽带运营商处理',
+                    // btnStr: '设置上网方式',
+                    problem: intl.get(MODULE, 11),
+                    reason: intl.get(MODULE, 12),
+                    solution: intl.get(MODULE, 13),
+                    btnStr: intl.get(MODULE, 14),
                     solutionFunc: this.setWan,
                 });
                 return;
@@ -135,7 +153,8 @@ export default class Diagnose extends React.Component {
                 return;
             }
         } else {
-            Modal.error({ title: '网络诊断失败', content: message, centered: true });
+            // Modal.error({ title: '网络诊断失败', content: message, centered: true });
+            Modal.error({ title: intl.get(MODULE, 15), content: message, centered: true });
         }
     }
 
@@ -181,7 +200,7 @@ export default class Diagnose extends React.Component {
 
     render() {
         const { status, step, problem, reason, solution, btnStr, solutionFunc, time, bgIcon } = this.state;
-        const timeTip = `网络已联通，即将返回首页（${time}秒）...`;
+        const timeTip = intl.get(MODULE, 16, {time});
         const dot = [0, 1, 2].map(i => {
             if (step === i) {
                 return <Icon type={'loading'} style={{ fontSize: '0.85em' }}/>;
@@ -195,36 +214,36 @@ export default class Diagnose extends React.Component {
         return (
             <div className='diagnose-con'>
                 <div className='doing' style={{display:('doing' === status ? 'block' : 'none')}}>
-                    <p className='title'>正在进行故障诊断，请稍候...</p>
+                    <p className='title'>{intl.get(MODULE, 18)}</p>
                     <Timeline className='time-line'>
-                        <Timeline.Item dot={dot[0]} className={0 == step ? 'now':''}>检测网线连接状态</Timeline.Item>
-                        <Timeline.Item dot={dot[1]} className={1 == step ? 'now' : ''}>检测WAN口拨号</Timeline.Item>
-                        <Timeline.Item dot={dot[2]} className={2 == step ? 'now' : ''}>检测Internet服务</Timeline.Item>
+                        <Timeline.Item dot={dot[0]} className={0 == step ? 'now':''}>{intl.get(MODULE, 19)}</Timeline.Item>
+                        <Timeline.Item dot={dot[1]} className={1 == step ? 'now' : ''}>{intl.get(MODULE, 20)}</Timeline.Item>
+                        <Timeline.Item dot={dot[2]} className={2 == step ? 'now' : ''}>{intl.get(MODULE, 21)}</Timeline.Item>
                     </Timeline>
-                    <Button className='diagnose-btn' onClick={this.goBack}>取消检测</Button>
+                    <Button className='diagnose-btn' onClick={this.goBack}>{intl.get(MODULE, 22)}</Button>
                 </div>
                 <div className='warning' style={{ display: ('warning' === status ? 'block' : 'none') }}>
-                    <p className='title'>诊断完成</p>
+                    <p className='title'>{intl.get(MODULE, 23)}</p>
                     <div className='result'>
-                        <label>诊断问题：</label><span>{problem}</span>
+                        <label>{intl.get(MODULE, 24)}</label><span>{problem}</span>
                     </div>
                     <div className='reason'>
-                        <label>可能原因：</label><span>{reason}</span>
+                        <label>{intl.get(MODULE, 25)}</label><span>{reason}</span>
                     </div>
                     <div className='solution'>
-                        <label>解决办法：</label><span>{solution}</span>
+                        <label>{intl.get(MODULE, 26)}</label><span>{solution}</span>
                     </div>
                     <Button className='diagnose-btn' onClick={solutionFunc}>{btnStr}</Button>
                 </div>
                 <div className='okay' style={{ display: ('okay' === status ? 'block' : 'none') }}>
-                    <p className='title'>诊断完成</p>
+                    <p className='title'>{intl.get(MODULE, 27)}</p>
                     <div className='result'>
-                        <label>诊断问题：</label><span>未发现问题</span>
+                        <label>{intl.get(MODULE, 28)}</label><span>{intl.get(MODULE, 29)}</span>
                     </div>
                     <div className='solution'>
                         <label>{timeTip}</label>
                     </div>
-                    <Button className='diagnose-btn' onClick={this.goBack}>立即返回</Button>
+                    <Button className='diagnose-btn' onClick={this.goBack}>{intl.get(MODULE, 30)}</Button>
                 </div>
                 <div className='bg-icon'><CustomIcon type={bgIcon} size={720} /></div>
                 <div className='bg-icon2'><CustomIcon type={bgIcon} size={720} /></div>
