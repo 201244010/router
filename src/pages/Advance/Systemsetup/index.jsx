cd003e438b282b144943432d5464eb234cc40e9d
liujun <3038855308@qq.com>
2019-03-01 18:45:09 +0800

    根据版本区分系统设置中的时区显示

--- src/pages/Advance/Systemsetup/index.jsx
+++ src/pages/Advance/Systemsetup/index.jsx
@@ -14,18 +14,46 @@ const MODULE = 'systemsetup';
 const TabPane = Tabs.TabPane;
 
 export default class Systemsetup extends React.PureComponent {
+    getVersion = () => {
+        let QUICK_SETUP = JSON.parse(window.sessionStorage.getItem('QUICK_SETUP'));     //获取版本信息
+
+        if (3 === QUICK_SETUP.length) {             //根据快速设置的步骤数，判断是国内版还是海外版
+            return "domestic";
+        }
+
+        if (4 === QUICK_SETUP.length) {
+            return "abroad";
+        }
+    }
+
     render(){
-        return (
-            <Tabs>
-                <TabPane tab={intl.get(MODULE, 0)/*_i18n:安全设置*/} key="1"><Dosd /></TabPane>
-                <TabPane tab={intl.get(MODULE, 1)/*_i18n:修改管理密码*/} key="2"><ChangePassword /></TabPane>
-                <TabPane tab={intl.get(MODULE, 2)/*_i18n:系统升级*/} key="3"><SysUpgrade /></TabPane>
-                <TabPane tab={intl.get(MODULE, 3)/*_i18n:备份与恢复*/} key="4"><Backup /></TabPane>
-                <TabPane tab={intl.get(MODULE, 6)/*_i18n:时区/时间*/} key="5"><TimeZone /></TabPane>
-                <TabPane tab={intl.get(MODULE, 4)/*_i18n:重启路由器*/} key="6"><Reboot /></TabPane>
-                <TabPane tab={intl.get(MODULE, 5)/*_i18n:恢复出厂设置*/} key="7"><Recovery /></TabPane>
-            </Tabs>
-        );
+        if ('domestic' === this.getVersion()) {
+            return (
+                <Tabs>
+                    <TabPane tab={intl.get(MODULE, 0)/*_i18n:安全设置*/} key="1"><Dosd /></TabPane>
+                    <TabPane tab={intl.get(MODULE, 1)/*_i18n:修改管理密码*/} key="2"><ChangePassword /></TabPane>
+                    <TabPane tab={intl.get(MODULE, 2)/*_i18n:系统升级*/} key="3"><SysUpgrade /></TabPane>
+                    <TabPane tab={intl.get(MODULE, 3)/*_i18n:备份与恢复*/} key="4"><Backup /></TabPane>
+                    <TabPane tab={intl.get(MODULE, 4)/*_i18n:重启路由器*/} key="6"><Reboot /></TabPane>
+                    <TabPane tab={intl.get(MODULE, 5)/*_i18n:恢复出厂设置*/} key="7"><Recovery /></TabPane>
+                </Tabs>
+            );
+        }
+
+        if ('abroad' === this.getVersion()) {
+            return (
+                <Tabs>
+                    <TabPane tab={intl.get(MODULE, 0)/*_i18n:安全设置*/} key="1"><Dosd /></TabPane>
+                    <TabPane tab={intl.get(MODULE, 1)/*_i18n:修改管理密码*/} key="2"><ChangePassword /></TabPane>
+                    <TabPane tab={intl.get(MODULE, 2)/*_i18n:系统升级*/} key="3"><SysUpgrade /></TabPane>
+                    <TabPane tab={intl.get(MODULE, 3)/*_i18n:备份与恢复*/} key="4"><Backup /></TabPane>
+                    <TabPane tab={intl.get(MODULE, 6)/*_i18n:时区/时间*/} key="5"><TimeZone /></TabPane>
+                    <TabPane tab={intl.get(MODULE, 4)/*_i18n:重启路由器*/} key="6"><Reboot /></TabPane>
+                    <TabPane tab={intl.get(MODULE, 5)/*_i18n:恢复出厂设置*/} key="7"><Recovery /></TabPane>
+                </Tabs>
+            );
+        } 
+        
     }
 };
 
