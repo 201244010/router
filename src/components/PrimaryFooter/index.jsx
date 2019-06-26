616e16237a7548ac6c471ff922369adcc7c1d84e
zhancaiqun <zhancaiqun@sunmi.com>
2019-06-26 17:19:32 +0800

    解决组网版初配之后版本信息获取不到

--- src/components/PrimaryFooter/index.jsx
+++ src/components/PrimaryFooter/index.jsx
@@ -22,7 +22,7 @@ export default class PrimaryFooter extends React.PureComponent {
     fetchFooterInfo = async () => {
         await common.fetchApi(
             [
-                {opcode : 'MESH_FIRMWARE_GET'},
+                {opcode : 'FIRMWARE_GET'},
                 {opcode :'NETWORK_WAN_IPV4_GET' },
                 {opcode : 'ROUTE_GET'},
             ]
@@ -36,15 +36,8 @@ export default class PrimaryFooter extends React.PureComponent {
                     }
                 });
 
-                let version = '';
-                data[0].result.upgrade.map(item => {
-                    if (item.devid === devId) {
-                        version = item.current_version;
-                    }
-                })
-
                 this.setState({
-                    version : version,
+                    version : data[0].result.upgrade.current_version,
                     mac : data[1].result.wan.info.mac
                 })
             }
