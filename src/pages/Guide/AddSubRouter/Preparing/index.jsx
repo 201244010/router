070b86a5adcf8858eff030f6b97aa71a32fcd26d
liujun <3038855308@qq.com>
2019-10-12 10:15:03 +0800

    【W1修改】添加子路由完成，删掉不必要的Finish(之前的改版)

--- src/pages/Guide/AddSubRouter/Preparing/index.jsx
+++ src/pages/Guide/AddSubRouter/Preparing/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from 'antd';
+import CustomIcon from '~/components/Icon';
 
 import './preparing.scss';
 const MODULE = 'preparing';
@@ -16,10 +17,14 @@ export default class Preparing extends React.Component {
     render() {
         return (
             <div className="preparing">
-                <h2>{intl.get(MODULE, 0)/*_i18n:子路由设置准备*/}</h2> 
-                <p className="ui-tips guide-tip">{intl.get(MODULE, 1)/*_i18n:将所有子路由放置在合适的位置，两个路由之间距离不要超过两个房间，然后接通电源，待子路由的信号灯呈白色后点击「下一步」。*/}</p>
+                <h2>{intl.get(MODULE, 0)/*_i18n:组网设置准备*/}</h2>
                 <div className="content">
                     <img className='image' src={require('~/assets/images/guide-router.png')}/>
+                    <div className='attention'>
+                        <p>{intl.get(MODULE, 3)/*_i18n:1. 将需要加入网络的路由器接通电源，待系统指示灯*/}<CustomIcon size={20} color="rgba(51,60,79,1)" type="Internet"/>{intl.get(MODULE, 4)/*_i18n:停止闪烁，即启动完成；*/}</p>
+                        <p>{intl.get(MODULE, 5)/*_i18n:2. 若需要加入网络的路由器已被配置过，请先长按路由器背面Reset孔5秒以上，将之恢复出厂设置后重新启动；*/}</p>
+                        <p>{intl.get(MODULE, 6)/*_i18n:3. 点击“开始设置”*/}</p>
+                    </div>
                     <Button type="primary" className="next" onClick={this.next}>{intl.get(MODULE, 2)/*_i18n:下一步*/}</Button>
                 </div>
             </div>
