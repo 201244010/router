46201682cb65706aca199bd21b7da15fd981b83d
liujun <3038855308@qq.com>
2019-10-14 18:01:45 +0800

    【W1修改】上网设置修改完成

--- src/pages/Settings/Network/Dns.jsx
+++ src/pages/Settings/Network/Dns.jsx
@@ -9,30 +9,47 @@ class Dns extends React.Component  {
 		const {dnsname, dnsTip, dns, dnsbackupname, dnsbackupTip, dnsbackup, onChange} = this.props;
 		const list = [
 			{
-				label: intl.get(MODULE, 46)/*_i18n:首选DNS*/,
-				key: dnsname,
-				tip: dnsTip,
-				value: dns,
-			},
-			{
-				label: intl.get(MODULE, 47)/*_i18n:备选DNS（选填）*/,
-				key: dnsbackupname,
-				tip: dnsbackupTip,
-				value: dnsbackup,
-			},
+				left: {
+					label: intl.get(MODULE, 46)/*_i18n:首选DNS*/,
+					key: dnsname,
+					tip: dnsTip,
+					value: dns,
+				},
+				right: {
+					label: intl.get(MODULE, 47)/*_i18n:备选DNS（选填）*/,
+					key: dnsbackupname,
+					tip: dnsbackupTip,
+					value: dnsbackup,
+				},
+			}
 		];
         return (
             <div className="wifi-settings">
 				{
 					list.map(item => (
-						<React.Fragment key={item.key}>
-							<label>{item.label}</label>
-							<FormItem key={item.key} showErrorTip={item.tip} style={{ width : 320}}>
-								<InputGroup
-									inputs={[{value: item.value[0], maxLength: 3}, {value: item.value[1], maxLength: 3}, {value: item.value[2], maxLength: 3}, {value: item.value[3], maxLength: 3}]} 
-									onChange={value => onChange(value, item.key)} />
-								<ErrorTip>{item.tip}</ErrorTip>
-							</FormItem>
+						<React.Fragment key={item.left.key}>
+							<div className='network-row'>
+								<div>
+									<label>{item.left.label}</label>
+									<FormItem key={item.left.key} showErrorTip={item.left.tip} style={{ width : 320}}>
+										<InputGroup
+											inputs={[{value: item.left.value[0], maxLength: 3}, {value: item.left.value[1], maxLength: 3}, {value: item.left.value[2], maxLength: 3}, {value: item.left.value[3], maxLength: 3}]} 
+											onChange={value => onChange(value, item.left.key)} />
+										<ErrorTip>{item.left.tip}</ErrorTip>
+									</FormItem>
+								</div>
+								{
+									item.right&&<div className='row-right'>
+										<label>{item.right.label}</label>
+										<FormItem key={item.right.key} showErrorTip={item.right.tip} style={{ width : 320}}>
+											<InputGroup
+												inputs={[{value: item.right.value[0], maxLength: 3}, {value: item.right.value[1], maxLength: 3}, {value: item.right.value[2], maxLength: 3}, {value: item.right.value[3], maxLength: 3}]} 
+												onChange={value => onChange(value, item.right.key)} />
+											<ErrorTip>{item.right.tip}</ErrorTip>
+										</FormItem>
+									</div>
+								}
+							</div>
 						</React.Fragment>
 					))
 				}
