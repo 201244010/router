f300e5b828ba5c2b58e7d474d79a2a2594ecbef2
liujun <3038855308@qq.com>
2019-12-30 18:39:34 +0800

    修改移动网络模块

--- src/pages/Advance/MobileNetwork/index.jsx
+++ src/pages/Advance/MobileNetwork/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Select, message } from 'antd';
+import { checkStr } from '~/assets/common/check';
 import SubLayout from '~/components/SubLayout';
 import PanelHeader from '~/components/PanelHeader';
 import Form from "~/components/Form";
@@ -7,7 +8,7 @@ import './index.scss';
 
 const MODULE = 'mobileNetwork';
 const Option = Select.Option;
-const {FormItem, Input} = Form;
+const {FormItem, Input, ErrorTip} = Form;
 
 class MobileNetwork extends React.PureComponent {
 	constructor(props) {
@@ -22,6 +23,10 @@ class MobileNetwork extends React.PureComponent {
 			auth: '',
 			authOptions: ['pap','chap','both','none'],
 			apn: '',
+			fileName: '',
+			userName: '',
+			password: '',
+			disabled: false,
 		};
 	}
 
@@ -33,7 +38,7 @@ class MobileNetwork extends React.PureComponent {
 		const response = await common.fetchApi([{ opcode: 'MOBILE_CONFIG_GET' }]);
         const { errcode, data } = response;
         if(errcode === 0){
-			const { mobile: { switch: enable, status, mode, iptype: ipType, auth, apn } = {} } = data[0].result;
+			const { mobile: { switch: enable, status, mode, iptype: ipType, auth, apn, user, passwd, profile } = {} } = data[0].result;
 			this.setState({
 				enable: enable === 'on',
 				status,
@@ -44,6 +49,9 @@ class MobileNetwork extends React.PureComponent {
 				auth: auth.cfg,
 				authOptions: auth.list,
 				apn,
+				fileName: profile,
+				userName: user,
+				password: passwd,
 			});
 		} else {
 			message.error(intl.get(MODULE, 10)/*_i18n:配置获取失败*/);
@@ -51,7 +59,7 @@ class MobileNetwork extends React.PureComponent {
 	}
 
 	setMobileConfig = async() => {
-		const { enable, mode, ipType, auth } = this.state;
+		const { enable, mode, ipType, auth, userName, password, fileName } = this.state;
 
 		const response = await common.fetchApi([{
 			opcode: 'MOBILE_CONFIG_SET',
@@ -61,6 +69,9 @@ class MobileNetwork extends React.PureComponent {
 					mode,
 					iptype: ipType,
 					auth,
+					user: userName,
+					passwd: password,
+					profile: fileName,
 				},
 			}
 		}]);
@@ -78,28 +89,68 @@ class MobileNetwork extends React.PureComponent {
 	}
 
 	render() {
-		const { enable, status, mode, modeOptions, ipType, ipTypeOptions, auth, authOptions, apn } = this.state;
+		const { enable, status, mode, modeOptions, ipType, ipTypeOptions, auth, authOptions, apn, fileName, userName, password, disabled } = this.state;
 
 		let netStatus = status === ''? '' : status === 'online'? intl.get(MODULE, 2)/*_i18n:已连接*/: intl.get(MODULE, 3)/*_i18n:未连接*/;
 		netStatus = enable? netStatus : intl.get(MODULE, 13)/*_i18n:已关闭*/;
 
+		const tip = checkStr(apn, { who: intl.get(MODULE, 7)/*_i18n:APN*/, min: 1, byte: true });
 		const dialInfo = [
-			{
-				label: intl.get(MODULE, 5)/*_i18n:IP类型*/,
-				selectId: 'ipTypeArea',
-				value: ipType,
-				type: 'ipType',
-				options: ipTypeOptions,
-
-			},
-			{
-				label: intl.get(MODULE, 6)/*_i18n:认证方式*/,
-				selectId: 'authArea',
-				value: auth,
-				type: 'auth',
-				options: authOptions,
-			},
+			[
+				{
+					label: 'Profile名称（选填）',
+					value: fileName,
+					type: 'fileName',
+					inputType: 'input',
+					disabled,
+				},
+				{
+					label: intl.get(MODULE, 5)/*_i18n:IP类型*/,
+					value: ipType,
+					type: 'ipType',
+					selectId: 'ipTypeArea',
+					options: ipTypeOptions,
+					inputType: 'select',
+					disabled,
+				},
+			],
+			[
+				{
+					label: intl.get(MODULE, 6)/*_i18n:认证方式*/,
+					value: auth,
+					type: 'auth',
+					selectId: 'authArea',
+					options: authOptions,
+					inputType: 'select',
+					disabled,
+				},
+				{
+					label: intl.get(MODULE, 7)/*_i18n:APN*/,
+					value: apn,
+					type: 'apn',
+					inputType: 'input',
+					disabled,
+					tip,
+				},
+			],
+			[
+				{
+					label: '用户名（选填）',
+					value: userName,
+					type: 'userName',
+					inputType: 'input',
+					disabled,
+				},
+				{
+					label: '密码（选填）',
+					value: password,
+					type: 'password',
+					inputType: 'input',
+					disabled,
+				},
+			],
 		];
+		const buttonDisabled = enable? (disabled || tip !== ''): false;
 		return (
 			<SubLayout className="settings">
 				<Form className='mobile-form'>
@@ -119,6 +170,7 @@ class MobileNetwork extends React.PureComponent {
 								style={{ width: 320 }}
 								onChange={(value)=>this.onChange('mode',value)}
 								getPopupContainer={() => document.getElementById('netModeArea')}
+								disabled={disabled}
 							>
 								{modeOptions.map(item => (
 									<Option value={item} key={item}>{item}</Option>
@@ -127,34 +179,46 @@ class MobileNetwork extends React.PureComponent {
 						</div>
 						<p className='mobile-dilInfo'>{intl.get(MODULE, 9)/*_i18n:拨号信息*/}</p>
 						{dialInfo.map(item => (
-							<React.Fragment key={item.label}>
-								<label className='mobile-label'>{item.label}</label>
-								<div className='mobile-selectArea' id={item.selectId}>
-									<Select
-										value={item.value}
-										style={{ width: 320 }}
-										onChange={(value)=>this.onChange(item.type,value)}
-										getPopupContainer={() => document.getElementById(item.selectId)}
-										disabled
-									>
-										{item.options.map(item => (
-											<Option value={item} key={item}>{item}</Option>
-										))}
-									</Select>
-								</div>
-							</React.Fragment>
+							<div className='mobile-dilInfo-list'>
+								{item.map((item, index) => (
+									item.inputType === 'select'?
+									<div className={index > 0? 'dilInfo-list-right':''} key={item.label}>
+										<label className='mobile-label'>{item.label}</label>
+										<div className='mobile-selectArea' id={item.selectId}>
+											<Select
+												value={item.value}
+												style={{ width: 320 }}
+												onChange={(value)=>this.onChange(item.type,value)}
+												getPopupContainer={() => document.getElementById(item.selectId)}
+												disabled={disabled}
+											>
+												{item.options.map(item => (
+													<Option value={item} key={item}>{item}</Option>
+												))}
+											</Select>
+										</div>
+									</div>
+									:
+									<div className={index > 0? 'dilInfo-list-right':''} key={item.label}>
+										<label className='mobile-label'>{item.label}</label>
+										<FormItem className='mobile-formItem' showErrorTip={item.tip} type="small">
+											<Input type="text" value={item.value} disabled={disabled} onChange={value => {this.onChange(item.type, value)}}/>
+											<ErrorTip>{item.tip}</ErrorTip>
+										</FormItem>
+									</div>
+								))
+								}
+							</div>
+							
 						))}
-						<label className='mobile-label'>{intl.get(MODULE, 7)/*_i18n:APN*/}</label>
-						<FormItem className='mobile-formItem' type="small">
-							<Input type="text" value={apn} disabled onChange={value => {this.onChange('apn', value)}}/>
-						</FormItem>
+						
 					</div>}
 				</Form>
 				<section className="save-area">
 					<Button
 						size="large"
 						type="primary"
-						disabled={false}
+						disabled={buttonDisabled}
 						onClick={this.setMobileConfig}
 						className='button'
 					>{intl.get(MODULE, 8)/*_i18n:保存*/}</Button>
